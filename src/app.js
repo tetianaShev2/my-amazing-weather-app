fba55e626cf79cf8526551deabfc10a653cf0eb6
tetianaShev2 <105647410+tetianaShev2@users.noreply.github.com>
2022-07-10 22:40:38 +0200

    added search engine in JS

--- src/app.js
+++ src/app.js
@@ -34,10 +34,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "1b92415f0f2a81ca6f05973c2626e33c";
+  let apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1b92415f0f2a81ca6f05973c2626e33c&units=metric";
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "1b92415f0f2a81ca6f05973c2626e33c";
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=1b92415f0f2a81ca6f05973c2626e33c&units=metric";
-
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
