95fd1f96dacf83694bfc9d130f1d03033183a8e2
tetianaShev2 <105647410+tetianaShev2@users.noreply.github.com>
2022-07-10 17:09:26 +0200

    updated date and time

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return day + " " + hours + ":" + minutes;
+}
 
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "1b92415f0f2a81ca6f05973c2626e33c";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=1b92415f0f2a81ca6f05973c2626e33c&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
